05c296acd90e8eed4a1988241edbfc6c5fce7946
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-06-24 16:32:27 +0300

    react sass konusu işlendi.

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import headerStyle from "../scss/header.module.scss";
+
+const Header = () => {
+  return (
+    <div>
+    <header>
+        <h1 className={headerStyle["title"]}>COMMENTS</h1>
+    </header>
+        
+    </div>
+  )
+}
+
+export default Header
